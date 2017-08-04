a75ffcdfced0e262869b5f585490afdc2fc2e586
Ethan Shumate <ethanshumate@gmail.com>
2017-08-04 15:14:27 -0400

    Merging everyones builds
    Merging everyones builds

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class CameraControls : MonoBehaviour {
 
-    public Vector3 offset;
+    Vector3 offset = new Vector3(0,6,-11);
+    Vector3 rotation = new Vector3(22,0,0);
     public Transform player;
 
-	void Update () {
+    private void Start()
+    {
+        transform.Rotate(rotation);
+    }
+    void Update () {
         transform.position = player.transform.position + offset;
 	}
 }
---
+++ Assets/Scripts/JoystickMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickMovement : MonoBehaviour {
+
+    float speed = 7f;
+    public float turnSpeed;
+    
+
+    void Update()
+    {
+        var moveHorizontal = Input.GetAxis("JLHorizontal") * Time.deltaTime * speed;
+        var moveVertical = Input.GetAxis("JLVertical") * Time.deltaTime * speed;
+
+        transform.Translate(moveHorizontal, 0, moveVertical, Space.World);
+
+
+        Vector3 NextDir = new Vector3(Input.GetAxisRaw("JRHorizontal"), 0, Input.GetAxisRaw("JRVertical"));
+        if (NextDir != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(NextDir);
+            Shoot();
+        }
+    }
+    public GameObject projectile;
+    public float fireRate;
+    float nextFire;
+    void Shoot()
+    {
+        if(Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(projectile, transform.position + transform.forward, transform.rotation);
+        }
+        
+
+    }
+}
