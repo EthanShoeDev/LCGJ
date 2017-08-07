48b9b588e2b9011e9358036de83701446a57046c
uNitYAmAture <30784402+uNitYAmAture@users.noreply.github.com>
2017-08-07 18:30:46 -0400

    NEW BUILD
    ITS A NEW BUILD>>>>> PICKLE RICKKKKKK!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ Assets/Assets/Assets/enemy.js
@@ -0,0 +1,6 @@
+﻿var player_object : Transform;
+function update (){
+	renderer.material.color = Color.red;
+	transform.LookAt(player_object.transfrom);
+	transform.translate(Vector3(0,0,0.08));
+}
---
+++ Assets/Assets/Assets/specialAttack.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+var specialDmg = 200;
+
+function Start () {
+
+}
+
+//function Update () {
+// if(Input.GetKeyDown(KeyCode.A)){
+   //
+  //   }
+     
+     //if(Input.GetKeyUp(KeyCode.A)){
+       //
+     //}
+//}
