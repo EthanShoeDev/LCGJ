4b2bf474a43be6ea474d8c3eeed17284bfccecf1
Ethan Shumate <ethanshumate@gmail.com>
2017-08-09 20:47:08 -0400

    A* Pathfinding

--- Assets/Scripts/Skeleton.cs
+++ Assets/Scripts/Skeleton.cs
@@ -1,60 +1,106 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Pathfinding;
 using UnityEngine;
 
-public class Skeleton : MonoBehaviour {
+public class Skeleton : MonoBehaviour
+{
+    //The point to move to
+    public Transform target;
 
-    public float MaxSpeed = 3f;
-    [HideInInspector] public Vector3 DirectionFacing;
+    private Seeker seeker;
+
+    //The calculated path
+    public Path path;
+
+    //The AI's speed per second
+    public float speed = 2;
+
+    //The max distance from the AI to a waypoint for it to continue to the next waypoint
+    public float nextWaypointDistance = 3;
+
+    public float repathRate = 0.25f;
+
+    //The waypoint we are currently moving towards
+    private int currentWaypoint = 0;
 
     private Animator anim;
+    private float lastRepath = -999f;
 
-    // Use this for initialization
-    void Start()
+    public void Start()
     {
+        seeker = GetComponent<Seeker>();
         anim = GetComponent<Animator>();
+        //Start a new path to the targetPosition, return the result to the OnPathComplete function
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+        StartCoroutine(RepeatTrySearchPath());
     }
 
-    // Update is called once per frame
+    private IEnumerator RepeatTrySearchPath()
+    {
+        while (true) yield return new WaitForSeconds(TrySearchPath());
+    }
+
+    public float TrySearchPath()
+    {
+        if (Time.time - lastRepath >= repathRate && target != null)
+        {
+            SearchPath();
+            return repathRate;
+        }
+        else
+        {
+            float v = repathRate - (Time.time - lastRepath);
+            return v < 0 ? 0 : v;
+        }
+    }
+
+    private void SearchPath()
+    {
+        if (target == null) throw new System.InvalidOperationException("Target is null");
+        lastRepath = Time.time;
+        Vector3 targetPosition = target.position;
+        seeker.StartPath(transform.position, targetPosition, OnPathComplete);
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("Yay, we got a path back. Did it have an error? " + p.error);
+        if (!p.error)
+        {
+            path = p;
+            //Reset the waypoint counter
+            currentWaypoint = 0;
+        }
+    }
+
     void Update()
     {
-        Vector3 velocity = new Vector3
-        {
-            x = Input.GetAxis("Horizontal") * MaxSpeed * Time.deltaTime,
-            y = Input.GetAxis("Vertical") * MaxSpeed * Time.deltaTime,
-            z = transform.position.z
-        };
-        transform.position += velocity;
+        if (path == null)
+            return;
 
-        Vector3 scale = transform.localScale;
-        if (velocity.x < 0)
+        if (currentWaypoint >= path.vectorPath.Count)
         {
-            scale.x = -Mathf.Abs(scale.x);
-            DirectionFacing = Vector3.left;
-        }
-        else if (velocity.x > 0)
-        {
-            scale.x = Mathf.Abs(scale.x);
-            DirectionFacing = Vector3.right;
-        }
-        transform.localScale = scale;
-
-        //Need Sprite For When Wizard Faces Camera
-        if (velocity.y < 0)
-        {
-            DirectionFacing = Vector3.down;
-        }
-        else if (velocity.y > 0)
-        {
-            DirectionFacing = Vector3.up;
+            Debug.Log("End Of Path Reached");
+            return;
         }
 
-        if (velocity != Vector3.zero)
+        //Direction to the next waypoint
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+        dir *= speed * Time.deltaTime;
+        this.gameObject.transform.Translate(dir);
+
+        if (dir != Vector3.zero)
         {
-            if (Mathf.Abs(velocity.x) >= Mathf.Abs(velocity.y))
+            if (Mathf.Abs(dir.x) >= Mathf.Abs(dir.y))
                 anim.SetBool("isHorizontal", true);
             else
                 anim.SetBool("isHorizontal", false);
         }
+
+        //Check if we are close enough to the next waypoint
+        //If we are, proceed to follow the next waypoint
+        if (Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]) < nextWaypointDistance)
+            currentWaypoint++;
     }
 }
