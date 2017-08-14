68d6cd8fa1e583af4ea73f491977ffb3390d71bf
Ethan Shumate <ethanshumate@gmail.com>
2017-08-14 01:47:27 -0400

    fixed untracked files

---
+++ Assets/ColliderSwitcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimToCollider
+{
+    public GameObject ColliderObject;
+    public string AnimName;
+}
+
+public class ColliderSwitcher : MonoBehaviour {
+
+    public List<AnimToCollider> States;
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
